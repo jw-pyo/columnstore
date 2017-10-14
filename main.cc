28dea350d5364b080ac00ab2f8e7c6911bb36407
jw-pyo <wjddn1801@snu.ac.kr>
2017-10-14 16:21:30 +0900

    update

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include "Table.h"
 #include "util.h"
+#include "PackedArray.h"
 
 #include <iostream>
 #include <stdlib.h>
@@ -12,23 +13,22 @@ int main(){
 
 struct rusage usage;
 struct timeval start, end;
-string sensors_name[5] = {"data/sensors.csv", "sensors", "SID", "EID", "TYPE"};
-string entities_name[5] = {"data/entities.csv", "entities", "EID", "NAME", "TYPE"};
-string sample_game_name[15] = {"data/sample-game.csv", "sample-game", "SID", "TS", "X", "Y", "Z", "V", "A", "VX", "VY", "VZ", "AX", "AY", "AZ"};
+string sensors_name[8] = {"data/sensors.csv", "sensors", "SID", "unsigned int", "EID", "unsigned int", "TYPE", "unsigned int"};
+string entities_name[8] = {"data/entities.csv", "entities", "EID", "unsigned int", "NAME", "string", "TYPE", "unsigned int"};
+string sample_game_name[28] = {"data/sample-game.csv", "sample-game", "SID", "unsigned int", "TS", "double", "X", "int", "Y", "int", "Z", "int", "V", "unsigned int", "A", "unsigned int", "VX", "int", "VY", "int", "VZ", "int", "AX", "int", "AY", "int", "AZ", "int"};
 //string full_game_name[14] = {"sample-game", "SID", "TS", "X", "Y", "Z", "V", "A", "VX", "VY", "VZ", "AX", "AY", "AZ"};
-
 
 //Table_3<unsigned int, unsigned int, unsigned int>* t_sensors = new Table_3<unsigned int, unsigned int, unsigned int>(sensors_name, 42);
 //Table_3<unsigned int, string, unsigned int>* t_entities = new Table_3<unsigned int, string, unsigned int>(entities_name, 21);
 getrusage(RUSAGE_SELF, &usage);
 start = usage.ru_utime;
-Table_13<unsigned int, double, int, int, int, unsigned int, unsigned int, int, int, int, int, int, int>* t_sample_game = new Table_13<unsigned int, double, int, int, int, unsigned int, unsigned int, int, int, int, int, int, int>(sample_game_name, 1048576);
+Table* t_sensors_name = new Table(sensors_name, 42);
 getrusage(RUSAGE_SELF, &usage);
 end = usage.ru_utime;
 cout << "StartTime to load sample table: " << start.tv_sec << "." << start.tv_usec << endl;
 cout << "EndTime to load sample table: " << end.tv_sec << "." << end.tv_usec << endl;
 cout << "Use memory with table load: " << usage.ru_maxrss/(1024*1024) << " Mbytes" <<  endl;
-
+t_sensors.name->getValue(5);
 /*
 t_sample_game->col_0->edict->get_record(0);
 t_sample_game->col_1->edict->get_record(1);
@@ -51,8 +51,12 @@ t_sample_game->col_1->edict->get_record(17);
 t_sample_game->col_1->edict->get_record(18);
 t_sample_game->col_1->edict->get_record(19);
 */
-t_sample_game->sid_40();
-t_sample_game->v_less_5000000();
+t_sample_game->col_1->edict->print_record(500000,505000,false);
+t_sample_game->col_4->edict->print_record(500000,505000,false);
+t_sample_game->col_5->edict->print_record(500000,505000,false);
+t_sample_game->col_6->edict->print_record(500000,505000,false);
+//t_sample_game->sid_40();
+//t_sample_game->v_less_5000000();
 //t_sample_game->col_1->edict->print_record(10,20, false);
 //select('*', t_sample_game->col_0->equal(40), t_sample_game);
 
